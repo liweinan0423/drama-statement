acdfa4875b4fb3d6e338fd3481fb0517bd7fce39
Li Weinan <li.weinan@rea-group.com>
2019-07-10 14:27:17 +0800

    extract type code with subclass

--- src/main/java/Play.java
+++ src/main/java/Play.java
@@ -1,12 +1,13 @@
-class Play {
+abstract class Play {
     static Play create(String name, String type) {
         if ("tragedy".equals(type)) {
             return new Tragedy(name);
-        } else if ("comendy".equals(type)) {
+        } else if ("comedy".equals(type)) {
             return new Comedy(name);
         }
-        return new Play(name, type);
+        return null;
     }
+
     public static final int TRAGEDY_BASE_PRICE = 400;
     public static final int COMEDY_BASE_PRICE = 300;
     public static final int TRAGEDY_EXTRA_PRICE = 10;
@@ -14,19 +15,14 @@ class Play {
     public static final int COMEDY_EXTRA_PRICE = 5;
     public static final int COMEDY_EXTRA_FACTOR = 3;
     private String name;
-    private String type;
 
     public Play(String name, String type) {
         this.name = name;
-        this.type = type;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getType() {
-        return type;
-    }
-
+    public abstract String getType();
 }
