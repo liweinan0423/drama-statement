62908cbc209e2c073d160b728477f1e29deb7288
Li Weinan <li.weinan@rea-group.com>
2019-07-24 16:09:42 +0800

    introducing items into Invoice class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,10 +10,10 @@ class Invoice {
     List<InvoiceItem> invoiceItems = new ArrayList<>();
 
     void addItem(Performance performance, Play play) {
-        addItem(performance, play.getAmount(performance), performance.getAudiences(), play.getName());
+        addItem(play.getAmount(performance), performance.getAudiences(), play.getName());
     }
 
-    void addItem(Performance performance, int amount, int audiences, String playName) {
+    void addItem(int amount, int audiences, String playName) {
         InvoiceItem invoiceItem = new InvoiceItem();
         invoiceItem.amount = amount;
         invoiceItem.playName = playName;
