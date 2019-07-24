933ae41fc94b5ca087bd2f5c812fa1d2b9e98cbf
Li Weinan <li.weinan@rea-group.com>
2019-07-24 15:42:29 +0800

    push down calculateVolumeCredits method to concrete class

--- src/main/java/Tragedy.java
+++ src/main/java/Tragedy.java
@@ -12,4 +12,14 @@ public class Tragedy extends Play {
     public int getAmount(Performance performance) {
         return performance.getTragedyAmount();
     }
+
+    int calculateVolumeCredits(Performance performance) {
+        int volumeCredits = 0;
+        volumeCredits += Math.max(performance.getAudiences() - Performance.VOLUME_CREDITS_THRESHOLD, 0);
+
+        // add extra credit for every 5 comedy attendees;
+        if ("comedy" == getType())
+            volumeCredits += Math.floor(performance.getAudiences() / Performance.EXTRA_CREDIT_FACTOR);
+        return volumeCredits;
+    }
 }
