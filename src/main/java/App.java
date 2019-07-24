84c7930261d13715a310619e8fba9f71b04c2f2e
Li Weinan <li.weinan@rea-group.com>
2019-07-24 16:12:36 +0800

    extracting printing logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -43,26 +43,30 @@ class App {
 
         Invoice invoice = statement.getInvoice(plays);
 
+        return printInvoice(statement, invoice);
+    }
+
+    private String printInvoice(Statement statement, Invoice invoice) {
         StringBuilder resultBuilder = new StringBuilder();
-        makeHead(statement, resultBuilder);
-        makeLines(invoice, resultBuilder);
-        makeFoot(invoice.totalAmount, invoice.volumeCredits, resultBuilder);
+        printHead(statement, resultBuilder);
+        printLines(invoice, resultBuilder);
+        printFoot(invoice.totalAmount, invoice.volumeCredits, resultBuilder);
         return resultBuilder.toString();
     }
 
-    private void makeLines(Invoice invoice, StringBuilder resultBuilder) {
+    private void printLines(Invoice invoice, StringBuilder resultBuilder) {
         for (InvoiceItem item : invoice.invoiceItems) {
             resultBuilder.append(" ").append(item.playName).append(": $").append(item.amount).append(" (").append(item.audiences).append(" seats)\n");
         }
     }
 
 
-    private void makeFoot(int totalAmount, int volumeCredits, StringBuilder resultBuilder) {
+    private void printFoot(int totalAmount, int volumeCredits, StringBuilder resultBuilder) {
         resultBuilder.append("Amount owed is $").append(totalAmount).append("\n");
         resultBuilder.append("You earned ").append(volumeCredits).append(" credits\n");
     }
 
-    private void makeHead(Statement statement, StringBuilder resultBuilder) {
+    private void printHead(Statement statement, StringBuilder resultBuilder) {
         resultBuilder.append("Statement for ").append(statement.customer).append("\n");
     }
 }
