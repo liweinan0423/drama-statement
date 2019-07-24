2b85680da9aac890d200b6d17e92712d3e2a14ab
Li Weinan <li.weinan@rea-group.com>
2019-07-24 16:06:02 +0800

    introducing items into Invoice class

--- src/main/java/Statement.java
+++ src/main/java/Statement.java
@@ -6,11 +6,12 @@ public class Statement {
 
     Invoice getInvoice(Map<String, Play> plays) {
         Invoice invoice = new Invoice();
+        int volumeCredits = 0;
+        int totalAmount = 0;
         for (Performance performance : performances) {
             Play play = plays.get(performance.getPlayId());
-            invoice.volumeCredits += play.calculateVolumeCredits(performance);
-            invoice.totalAmount += play.getAmount(performance);
-
+            volumeCredits += play.calculateVolumeCredits(performance);
+            totalAmount += play.getAmount(performance);
             invoice.items.put(performance, play.getAmount(performance));
             InvoiceItem invoiceItem = new InvoiceItem();
             invoiceItem.amount = play.getAmount(performance);
@@ -18,6 +19,8 @@ public class Statement {
             invoiceItem.audiences = performance.getAudiences();
             invoice.invoiceItems.add(invoiceItem);
         }
+        invoice.volumeCredits = volumeCredits;
+        invoice.totalAmount = totalAmount;
         return invoice;
     }
 }
